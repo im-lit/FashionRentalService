ce580b55b1e37222485f3c74cfba973c2ae0d7ae
im-lit <chetdicondi123@gmail.com>
2023-10-28 18:53:47 +0700

    CORS update

---
+++ src/main/java/com/example/fashionrentalservice/config/SwaggerCorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.fashionrentalservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/swagger-ui.html")
+                .allowedOrigins("http://localhost:8080", "http://fashionrental.online:8080");// Chỉ cho phép từ tên miền của Swagger UI
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
